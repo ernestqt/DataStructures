eaaf47156ed9ae5a291c33f63b0e4874dfaf9af3
ernestqt <ernestqtptf@icloud.com>
2022-07-19 23:01:53 +0200

    First Segment Tree Implementation

---
+++ segment_tree.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
+
+class segTree:
+
+    def __init__(self):
+        pass
+
+    def put(self, i , v):
+        pass
+
+    def evaluate(self, l, r):
+        pass
+
+    
